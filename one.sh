29c6eba304968b2b8c7cc527a464495927a50dc7
codayon <codayon@proton.me>
2025-09-23 23:29:39 +0600

    refactor: code cleanup and small changes

--- one.sh
+++ one.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-set -e # exit if any command fails
 
-# Detect package manager and declare required packages
+set -euo pipefail
+
 if command -v pnpm >/dev/null 2>&1; then
 	pm="pnpm"
 	create_args=(create vite)
 	extra_args=(--template react --no-interactive)
 	extra_packages=(add tailwindcss @tailwindcss/vite)
+elif command -v yarn >/dev/null 2>&1; then
+    pm="yarn"
+    create_args=(create vite)
+    extra_args=(--template react --no-interactive)
+    extra_packages=(add tailwindcss @tailwindcss/vite)
 elif command -v npm >/dev/null 2>&1; then
 	pm="npm"
 	create_args=(create vite@latest)
@@ -17,29 +22,24 @@ else
 	exit 1
 fi
 
-# Determine app name
-if [[ -z "$1" ]]; then
+if [[ -z "${1-}" ]]; then
     read -p "Enter project name (default: my-app): " app_name
     app_name=${app_name:-my-app}
 else
     app_name=$1
 fi
 
-# Create the project, enter folder and install dependencies
 $pm "${create_args[@]}" "$app_name" "${extra_args[@]}"
 cd "$app_name"
 $pm install
 $pm "${extra_packages[@]}"
 
-# Necessary modifications
 rm -rf ./public ./README.md ./src/assets ./src/App.css
 sed -i "2a\import tailwindcss from '@tailwindcss/vite'" ./vite.config.js
-sed -i 's/react()/react(), tailwindcss()/' ./vite.config.js
-sed -i 's|svg+xml|x-icon|' ./index.html
-sed -i 's|/vite.svg|https://archlinux.org/favicon.ico|' ./index.html
+sed -i 's|react()|react(), tailwindcss()|' ./vite.config.js
+sed -i 's|/vite.svg|https://vite.dev/logo.svg|' ./index.html
 echo '@import "tailwindcss";' > ./src/index.css
 
-# Boilerplate code
 cat <<EOF > ./src/App.jsx
 const App = () => {
   return <h1>working</h1>;
